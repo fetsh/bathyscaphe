b0095355adb9837e6e229a1b9256785ba004458b
ilzoff <il.zoff@gmail.com>
2012-05-09 16:43:28 +0400

    parsing exeptions to tvdb added

--- lib/bathyscaphe/tvdb.rb
+++ lib/bathyscaphe/tvdb.rb
@@ -9,6 +9,8 @@ module Bathyscaphe
     def initialize filename
       if md = filename.match(TVREGEXP1) || md = filename.match(TVREGEXP2)
         @name = md[1].gsub(".", " ").strip
+        @name = "Castle" if @name =~ /Castle 2009/i
+        @name = "Missing (2012)" if @name =~ /Missing 2012/i
         @season = md[2].to_i.to_s
         @episode = md[3].to_i.to_s
       else
